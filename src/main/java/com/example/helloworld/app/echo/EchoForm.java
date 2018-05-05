9977c61b0ed99635205abf68e00d856063d711c5
Hidetoshi Imai <hidepin@gmail.com>
2018-05-05 19:43:11 +0900

    Update error handling.

--- src/main/java/com/example/helloworld/app/echo/EchoForm.java
+++ src/main/java/com/example/helloworld/app/echo/EchoForm.java
@@ -2,9 +2,14 @@ package com.example.helloworld.app.echo;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class EchoForm implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@NotNull
+	@Size(min = 1, max = 5)
 	private String name;
 
 	public String getName() {
